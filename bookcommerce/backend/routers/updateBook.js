bc6c78be382ff54bf3ca38be646b8eb5b1a249a4
Z3th <sethanmawen97@gmail.com>
2022-10-18 09:59:39 +0000

    Added function to update the existing picture

--- bookcommerce/backend/routers/updateBook.js
+++ bookcommerce/backend/routers/updateBook.js
@@ -4,11 +4,26 @@ const Book = require("../models/book");
 const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
-  const { title, description, picture, quantity, book_id, token } = req.body;
+  const { title, description, quantity, book_id, token } = req.body;
   const user = jwt.verify(token, process.env.JWT_SECRET);
+
+  // name of the Picture input is sampleFile
+  let samplePicture = req.files.file;
 
   if (user.role === "admin") {
     try {
+      samplePicture.mv(
+        `${__dirname}/uploads/${title}-${samplePicture.name}`,
+        async function (err) {
+          if (err) {
+            console.log(err);
+            return res.status(500).send({ msg: "Error occured" });
+          }
+        }
+      );
+
+      let picture = `/${title}-${samplePicture.name}`;
+
       const response = await Book.updateOne(
         { book_id },
         {
